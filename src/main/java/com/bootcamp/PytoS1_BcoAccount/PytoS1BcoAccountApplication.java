de81bc637f06bc95cf7ca3d960eac4e497267920
hectorperez <hperezva@emeal.nttdata.com>
2022-03-11 18:17:49 -0500

    Se agregó estructura inicial, springboot, maven.

---
+++ src/main/java/com/bootcamp/PytoS1_BcoAccount/PytoS1BcoAccountApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS1_BcoAccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PytoS1BcoAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PytoS1BcoAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/PytoS1_BcoAccount/PytoS1BcoAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.PytoS1_BcoAccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PytoS1BcoAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
